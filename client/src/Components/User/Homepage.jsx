e19d1d4059c25ac1caac0101ae89fb25d3a8e872
Adhil-404 <adhilh404@gmail.com>
2025-06-30 12:39:06 +0530

    adhil

--- client/src/Components/User/Homepage.jsx
+++ client/src/Components/User/Homepage.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import "../../Assets/Styles/Userstyles/Homepage.css"
-import HomepageProduct from './HomepageProduct';
-import UserNav from './Usernav';
-import UserFooter from './UserFooter'
-import ProductDetail from './ProductDetail';
-import ContactForm from './ContactForm';
+
 import Cart from './Cart';
 
 
