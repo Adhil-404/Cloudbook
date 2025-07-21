954b3a7f8580a0f8d7eb1a3352769ca635f10230
Adhil-404 <adhilh404@gmail.com>
2025-07-21 11:18:25 +0530

    push

--- client/src/Components/User/Usernav.jsx
+++ client/src/Components/User/Usernav.jsx
@@ -5,7 +5,6 @@ import '../../Assets/Styles/Userstyles/userNav.css';
 function UserNav() {
   return (
     <div>
-
       <div className="header-top">
         <div className="contact-info">
           <i className="bi bi-telephone"></i> <span>+(84) - 1800 - 4635</span>
@@ -17,7 +16,6 @@ function UserNav() {
           <i className="bi bi-twitter"></i>
         </div>
       </div>
-
 
       <div className="header-middle">
         <div className="logo">cloudbook</div>
@@ -33,18 +31,15 @@ function UserNav() {
           <button><i className="bi bi-search"></i></button>
         </div>
         <div className="middle-icons">
-          <span>login</span>
+          <span>Login</span>
           <i className="bi bi-person-fill"></i>
           <i className="bi bi-heart-fill"></i>
-          <Link to="/user/homepage/cart"><i className="bi bi-cart-fill"></i></Link>
-          <span>login</span><i class="bi bi-person-fill"></i>
-
-          <i class="bi bi-heart-fill"></i>
-          <Link to="/user/homepage/cart"><i class="bi bi-cart-fill"></i></Link>
+          <Link to="/user/homepage/cart">
+            <i className="bi bi-cart-fill"></i>
+          </Link>
         </div>
       </div>
 
-    
       <header className="homepage-header">
         <div className="header-bottom">
           <button className="categories-btn">☰ Categories</button>
@@ -56,15 +51,6 @@ function UserNav() {
           <Link to="/user/homepage/contact">Contact</Link>
           <Link to="/user/homepage/aboutus">About Us</Link>
         </nav>
-
-        <div>
-          <nav className="nav-links">
-            <a href="#">Home</a>
-            <Link to="/user/hompepage/product"> <a href="#">Products</a></Link>
-            <Link to="/user/hompepage/contact"><a href="#">Contact</a></Link>
-            <Link to="/user/homepage/aboutus"> <a href="#">About Us</a></Link>
-          </nav>
-        </div>
 
         <div className="support-info">
           <i className="bi bi-telephone"></i> <span>+1 840 - 841 25 69</span>
